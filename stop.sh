0f66410517642def346c64d03009d90da45ab5c3
palbungi@disroot.org <107607815+palbungi@users.noreply.github.com>
2025-08-09 02:25:27 +0900

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,15 @@
+# 서버 종료
+YAML_FILE="/home/bykim_bim/docker-compose.yml"
+CONTAINER_NAME="palworld"
+docker exec -i $CONTAINER_NAME rcon-cli save
+docker exec -i ${CONTAINER_NAME} rcon-cli "Broadcast Server_will_restart_in_5_seconds"
+sleep 1
+docker exec -i ${CONTAINER_NAME} rcon-cli "Broadcast Server_will_restart_in_4_seconds"
+sleep 1
+docker exec -i ${CONTAINER_NAME} rcon-cli "Broadcast Server_will_restart_in_3_seconds"
+sleep 1
+docker exec -i ${CONTAINER_NAME} rcon-cli "Broadcast Server_will_restart_in_2_seconds"
+sleep 1
+docker exec -i ${CONTAINER_NAME} rcon-cli "Broadcast Server_will_restart_in_1_seconds"
+docker-compose -f "${YAML_FILE}" pull
+docker-compose -f "${YAML_FILE}" down
